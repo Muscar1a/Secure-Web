e8bbbb0d911c6c764a3719eba5a0e39134e9d45b
Muscar1a <vn.annguyenit@gmail.com>
2025-04-23 14:36:52 +0700

    update password security

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel, Field
+import re
+from pydantic import BaseModel, field_validator
 
 class UserBase(BaseModel):
     username: str
@@ -7,9 +8,26 @@ class UserBase(BaseModel):
 class UserCreate(UserBase):
     password: str
 
+
+    @field_validator("password")
+    def password_complexity(cls, v: str) -> str:
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters long")
+        # must include letters
+        if not re.search(r"[a-z]", v):
+            raise ValueError("Password must include at least one lowercase letter")
+        # at least one special character
+        if not re.search(r"[!@#$%^&*(),.?\":{}|<>]", v):
+            raise ValueError("Password must include at least one special character")
+        # at least one uppercase letter
+        if not re.search(r"[A-Z]", v):
+            raise ValueError("Password must include at least one uppercase letter")
+        
+        return v
+    
+    
 class User(UserBase):
     id: str
     is_active: bool
     class Config:
         orm_mode = True
-
