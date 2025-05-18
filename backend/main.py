f019ebb0f7a121bff3d72a371b6c9e34bd1bdfd7
Muscar1a <vn.annguyenit@gmail.com>
2025-05-18 07:41:11 +0700

    add Cors

--- backend/main.py
+++ backend/main.py
@@ -37,9 +37,8 @@ async def shutdown_event():
 #   CORS
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["http://localhost:8000, http://localhost:3000"],
-    allow_origins=["*"],
-    
+    allow_origins=["https://localhost:8000", "127.0.0.1:8000"],
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
